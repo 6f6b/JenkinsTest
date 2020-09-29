95294e5d8ed347399442c5159d997a84b0d83a4b
liufeng <liufeng@postop.cn>
2020-09-29 10:05:28 +0800

    初始化

---
+++ demo/demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by liufeng on 2020/9/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ demo/run.sh
@@ -0,0 +1,3 @@
+xcodebuild clean
+xcodebuild
+
